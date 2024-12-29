c4440da00f7aa61e54f0b763cffbc4ba95af3197
MawuliMyth <166629209+MawuliMyth@users.noreply.github.com>
2024-12-29 18:50:33 +0000

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+// Color Constants
+const Color kActiveCardColour = Color(0xFF1D1E33);
+const Color kInactiveCardColour = Color(0xFF111328);
+const Color kBottomContainerColour = Color(0xFFEB1555);
+
+// Text Style Constants
+const TextStyle kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const TextStyle kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const TextStyle kBottomTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kResultTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: Color(0xFF24D876),
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bmi_calculator_app/screens/input_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+    );
+  }
+}
